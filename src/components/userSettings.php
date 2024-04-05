116aaf6701508c6d6db01f61c90176411cfbc4eb
Herman Sood <hermansood23@gmail.com>
2024-04-05 13:37:21 -0700

    Worked on user settings. Users can now update their username, email, password and profile picture if they choose on their account. Profile picture has some issues to be fixed.

--- src/components/userSettings.php
+++ src/components/userSettings.php
@@ -1,27 +1,27 @@
- <!DOCTYPE html>
- <html lang="en">
+<?php 
+?>
+<!DOCTYPE html>
+<html lang="en">
 <!-- User Settings Form -->
- <div id="user-settings-form">
+<div id="user-settings-form">
     <h2>User Settings</h2>
-    <form action="http://www.randyconnolly.com/tests/process.php" method="post">
-      <label for="edit-username">Edit Username:</label>
-      <input type="text" id="edit-username" name="edit-username" placeholder="Enter new username">
-      <br>
-      <label for="edit-password">Edit Password:</label>
-      <input type="password" id="edit-password" name="edit-password" placeholder="Enter new password">
-      <br>
-      <label for="edit-email">Edit Email:</label>
-      <input type="email" id="edit-email" name="edit-email" placeholder="Enter new email">
-      <br>
-      <label for="edit-visibility">Edit Visibility:</label>
-      <select id="edit-visibility" name="edit-visibility">
-        <option value="public">Public</option>
-        <option value="private">Private</option>
-      </select>
-      <label for="edit-pfp">Edit Profile Picture:</label>
-      <input type="file" id="edit-pfp" name="edit-pfp" accept="image/png, image/jpeg" />
-      <br>
-      <input type="submit" value="Submit User Changes">
+    <form id="userSettingsForm" action="<?php echo SITEROOT . 'updateUserSettings/updateUserSettings.php'; ?>" method="post" enctype="multipart/form-data">
+        <label for="edit-username">Edit Username:</label>
+        <input type="text" id="edit-username" name="edit-username" placeholder="Enter new username">
+        <br>
+        <label for="edit-password">Edit Password:</label>
+        <input type="password" id="edit-password" name="edit-password" placeholder="Enter new password">
+        <br>
+        <label for="edit-email">Edit Email:</label>
+        <input type="email" id="edit-email" name="edit-email" placeholder="Enter new email">
+        <br>
+        <label for="edit-pfp">Edit Profile Picture:</label>
+        <input type="file" id="edit-pfp" name="edit-pfp" accept="image/png, image/jpeg" />
+        <br>
+        <!-- Display the current profile picture -->
+        <img src="<?php echo SITEROOT . 'src/uploads/' . $profilePicture; ?>" alt="Current Profile Picture">
+        <br>
+        <input type="submit" value="Submit User Changes">
     </form>
-  </div>
- </html>
+</div>
+</html>
